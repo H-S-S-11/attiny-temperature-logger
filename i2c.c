d3abc3246ddacc0b2cbf550eb9683baaf0739ee6
H-S-S-11 <52609833+H-S-S-11@users.noreply.github.com>
2019-12-22 01:11:07 +0000

    Update i2c.c
    Changes the code to actively drive the line high rather than waiting for it to be pulled up

--- i2c.c
+++ i2c.c
@@ -19,29 +19,24 @@ uint8_t i2c_master_send_byte(uint8_t data){
     uint8_t ack = 0;
     uint8_t i;
     for (i = 0; i < 7; i++){
-        //release SCL
-        DDRB &= ~_BV(PB2);
-        //wait for SCL to go high
-        while(!(PINB & _BV(PB2))){}
-        //pull SCL low again
-        DDRB |= _BV(PB2);
+        //pull SCL high
+        PORTB |= _BV(PB2);
+        //pull SCL low
+        PORTB &= ~_BV(PB2);
         //shift data
         USICR |= _BV(USICLK);
     }
-    //release SCL
-    DDRB &= ~_BV(PB2);
-    //wait for SCL to go high
-    while(!(PINB & _BV(PB2))){}
+    //Pull SCL high
+    PORTB |= _BV(PB2);
+    
     //all data sent
     //ack cycle
     //pull SCL low
-    DDRB |= _BV(PB2);
+    PORTB &= ~_BV(PB2);
     //Release SDA
     DDRB &= ~_BV(PB0);
-    //release SCL
-    DDRB &= ~_BV(PB2);
-    //wait for SCL to go high
-    while(!(PINB & _BV(PB2))){}
+    //Pull SCL high
+    PORTB |= _BV(PB2);
     if(!(PINB & _BV(PB0))){
         ack = 1;
     }
@@ -53,10 +48,10 @@ uint8_t i2c_master_send_byte(uint8_t data){
 }
 
 void i2c_stop_condition(){
+    //pull SCL high
+    PORTB |= _BV(PB2);
     //release SCL
     DDRB &= ~_BV(PB2);
-    //wait for SCL to go high
-    while(!(PINB & _BV(PB2))){}
     //release SDA
     DDRB &= ~_BV(PB0);
 }
