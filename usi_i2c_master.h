2ceebab497b2ed4a27674c640cb46c5021ad47ab
H-S-S-11 <52609833+H-S-S-11@users.noreply.github.com>
2019-12-22 01:38:01 +0000

    Changed main code to use library
    Also added ATtiny85 to library

--- usi_i2c_master.h
+++ usi_i2c_master.h
@@ -49,6 +49,16 @@
     #define PIN_USI_SCL         PINB7
 #endif
 
+#if defined (__AVR_ATtiny85__)
+	#define DDR_USI			DDRB
+	#define PORT_USI		PORTB
+	#define PIN_USI			PINB
+	#define PORT_USI_SDA	PB0
+	#define PORT_USI_SCL	PB2
+	#define PIN_USI_SDA		PINB0
+	#define PIN_USI_SCL		PINB2
+#endif
+
 //USI I2C Master Transceiver Start
 // Starts the transceiver to send or receive data based on the r/w bit
 char USI_I2C_Master_Transceiver_Start(char *msg, char msg_size);
