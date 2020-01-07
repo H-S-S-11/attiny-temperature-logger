4679504fd795fe0524b082d6f1bfd8b0e914ffc0
H-S-S-11 <52609833+H-S-S-11@users.noreply.github.com>
2020-01-07 18:12:41 +0000

    Update temperature-log.c
    Start using 24FC512 libraries

--- ATtiny-sensor/temperature-log.c
+++ ATtiny-sensor/temperature-log.c
@@ -1,14 +1,15 @@
 #include <avr/power.h>
 #include "usi_i2c_master.h"
+#include "24FC512.h"
 
 int main(){
 
     clock_prescale_set(16);
-    char command[] = {0xa0, 0x00, 0x01, 0x1a, 0x2b, 0xc3, 0xd4, 0xe5, 0xf6};
+    char data[] = {0x00, 0x00, 0x00, 0x1a, 0x2b, 0xc3, 0xd4, 0xe5, 0xf6};
     
     while(1){
 
-        USI_I2C_Master_Start_Transmission(command, 9);
+        write_page_24xx512( 0x00, 0x0001, data, 9);
         _delay_ms(1000);
     }
 }
